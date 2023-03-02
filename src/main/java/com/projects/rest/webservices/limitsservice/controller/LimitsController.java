ea9ac03c49c553f78e980964647f182eefbd4d07
mtbren <bh.amit82@gmail.com>
2023-03-02 12:51:37 -0500

    Externalize properties

--- src/main/java/com/projects/rest/webservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/projects/rest/webservices/limitsservice/controller/LimitsController.java
@@ -1,14 +1,19 @@
 package com.projects.rest.webservices.limitsservice.controller;
 
+import com.projects.rest.webservices.limitsservice.config.Configuration;
 import com.projects.rest.webservices.limitsservice.entity.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping(path = "/limits")
     public Limits retrieveLimits(){
-        return new Limits(1,1000);
+        return new Limits(configuration.getMinimum(),configuration.getMaximum());
     }
 }
